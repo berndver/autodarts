ff710fa41e7654739f63d0ffbcd0213d8187cf65
berndver <49038133+berndver@users.noreply.github.com>
2024-07-19 12:06:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@ sudo apt install plasma-discover
 
 ## install docker
 curl -fsSL https://get.docker.com | sudo bash
- 
- ## grant access to docker.sock for watchtower run by user
+sudo apt-get install -y uidmap
+dockerd-rootless-setuptool.sh install
+sudo loginctl enable-linger $USER
+echo "export DOCKER_HOST=unix:///run/user/1000/docker.sock" >> ~/.bashrc
+
+## grant access to docker.sock for watchtower run by user
 sudo chmod 666 /var/run/docker.sock
