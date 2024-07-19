4dc7fffe386b4ad6b69298cc6a5c7daf1b930a59
berndver <49038133+berndver@users.noreply.github.com>
2024-07-19 12:17:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ echo "export DOCKER_HOST=unix:///run/user/1000/docker.sock" >> ~/.bashrc
 
 ## grant access to docker.sock for watchtower run by user
 sudo chmod 666 /var/run/docker.sock
+
+# optional: install vs code for editing files
+# sudo snap install code --classic
