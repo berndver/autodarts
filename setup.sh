9e85172e38fb3475d7ed7f33b3950719879c7715
berndver <49038133+berndver@users.noreply.github.com>
2024-07-19 11:57:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
-﻿## grant access to docker.sock for watchtower run by user
+## install packackge manager gui
+sudo apt install plasma-discover
+
+## install docker
+curl -fsSL https://get.docker.com | sudo bash
+ 
+ ## grant access to docker.sock for watchtower run by user
 sudo chmod 666 /var/run/docker.sock
