984d8d7ba7e2b74b20008fa92755939c155ed2a6
Bernd Verwohlt <mail@berndverwohlt.de>
2024-12-13 12:18:02 +0100

    :sparkles: add watchtower at  startup

--- auto-update.sh
+++ auto-update.sh
@@ -8,3 +8,6 @@ git pull origin main
 docker compose up --force-recreate -d
 cd collector
 docker compose up --force-recreate -d
+
+# run watchtower command
+docker run -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower -e WATCHTOWER_LABEL_ENABLE true --run-once
