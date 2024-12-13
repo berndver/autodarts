5898e6ed85498c4cd9cc67267b32712e4f3be69d
Bernd Verwohlt <mail@berndverwohlt.de>
2024-12-13 12:26:15 +0100

    fix

--- auto-update.sh
+++ auto-update.sh
@@ -9,8 +9,5 @@ docker compose up --force-recreate -d
 cd collector
 docker compose up --force-recreate -d
 
-# wait for containers to start
-sleep 10
-
 # run watchtower command
-docker run -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower -e WATCHTOWER_LABEL_ENABLE true --run-once
+docker run -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once --label-enable
