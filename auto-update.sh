9cb1e2a39529512f417a1625c9e2d73ca4ec8850
Bernd Verwohlt <mail@berndverwohlt.de>
2024-12-13 12:23:19 +0100

    :sparkles: add sleep to wait containers to start

--- auto-update.sh
+++ auto-update.sh
@@ -9,5 +9,8 @@ docker compose up --force-recreate -d
 cd collector
 docker compose up --force-recreate -d
 
+# wait for containers to start
+sleep 10
+
 # run watchtower command
 docker run -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower -e WATCHTOWER_LABEL_ENABLE true --run-once
